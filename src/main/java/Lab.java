fc78987ab04aa6031821c51a5d87fea7a6c78204
kobemelo <jgalarz95@gmail.com>
2023-02-14 19:33:14 +0000

    Committed If Statement

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,22 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+            x = 0;
+            y = 1;
+        if ( bool == true){
+            
+            return x;
+        }
+           
+
+       if (bool == false) {
+                  
+          return y;
+
+       }          
+         return 0;
+
+
     }
-}
+    } 
+
